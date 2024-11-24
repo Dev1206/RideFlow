eb677fa03fd04798f9ff91ce423b5b8b283ad3e4
Dev1206 <106542756+Dev1206@users.noreply.github.com>
2024-11-24 13:35:28 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
   try {
     console.log('Attempting to connect to MongoDB...');
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     console.log(`Database Name: ${conn.connection.name}`);
   } catch (error) {
