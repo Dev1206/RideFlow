2936c09e0b6996e76ba6691ebf4b447fe3aaaebb
Dev1206 <106542756+Dev1206@users.noreply.github.com>
2024-11-24 14:51:58 -0500

    Update LoadingSpinner.tsx
    placed loading spinner in center.

--- src/components/shared/LoadingSpinner.tsx
+++ src/components/shared/LoadingSpinner.tsx
@@ -1,5 +1,5 @@
 export const LoadingSpinner = () => (
-  <div className="flex items-center justify-center h-full">
+  <div className="fixed inset-0 flex items-center justify-center bg-white/50">
     <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
   </div>
-); 
+);
